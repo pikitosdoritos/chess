90a25f71639c6361e58b1d57e8078d14348ad56f
pikitosdoritos <fastovetsn@gmail.com>
2026-06-01 18:19:36 +0300

    Refactor board generation and rendering functions in main.py

--- main.py
+++ main.py
@@ -18,18 +18,15 @@ screen = pygame.display.set_mode((BOARD_SIZE + PADDING * 2, BOARD_SIZE + PADDING
 
 clock = pygame.time.Clock()
 
-board_rect = pygame.Rect(PADDING, PADDING, BOARD_SIZE, BOARD_SIZE)
-board_surface = screen.subsurface(board_rect)
-
 pygame.display.set_caption("CHESS")
 
-def generate_board(rows, cols):
+def generate_board():
     board = []
 
-    for row in range(rows):
+    for row in range(ROWS):
         squares = []
 
-        for col in range(cols):
+        for col in range(COLS):
             if (row + col) % 2 == 0:
                 squares.append("w")
             else:
@@ -39,7 +36,10 @@ def generate_board(rows, cols):
 
     return board
 
-def render_board(data):
+def render_board(data, screen):
+    board_rect = pygame.Rect(PADDING, PADDING, BOARD_SIZE, BOARD_SIZE)
+    board_surface = screen.subsurface(board_rect)
+
     for i, row in enumerate(data):
         for j, item in enumerate(row):
             color = LIGHT_COLOR if item == "w" else DARK_COLOR
@@ -49,7 +49,39 @@ def render_board(data):
 
             pygame.draw.rect(board_surface, color, (x, y, SQUARE_SIZE, SQUARE_SIZE))
 
-board = generate_board(ROWS, COLS)
+    render_labels(screen)
+
+def render_labels(surface):
+    font = pygame.font.Font(None, 50)
+
+    h_surface = pygame.Surface((BOARD_SIZE, PADDING), pygame.SRCALPHA)
+    v_surface = pygame.Surface((PADDING, BOARD_SIZE), pygame.SRCALPHA)
+
+    for i in range(ROWS):
+        letter = chr(97 + i)
+
+        text_surface = font.render(letter, True, LIGHT_COLOR)
+        rect = pygame.Rect(i * SQUARE_SIZE, 0, SQUARE_SIZE, PADDING)
+
+        text_rect = text_surface.get_rect(center=rect.center)
+
+        h_surface.blit(text_surface, text_rect)
+    
+    for j in range(COLS):
+        number_surface = font.render(str(j+1), True, LIGHT_COLOR)
+
+        rect = pygame.Rect(0, j * SQUARE_SIZE, PADDING, SQUARE_SIZE)
+
+        number_rect = number_surface.get_rect(center=rect.center)
+
+        v_surface.blit(number_surface, number_rect)
+
+    surface.blit(h_surface, (PADDING, 0))
+    surface.blit(h_surface, (PADDING, PADDING + BOARD_SIZE))
+    surface.blit(v_surface, (0, PADDING))
+    surface.blit(v_surface, (PADDING + BOARD_SIZE, PADDING))
+
+board = generate_board()
 
 while True:
     clock.tick(60)
@@ -61,6 +93,6 @@ while True:
 
     screen.fill(BG_COLOR)
 
-    render_board(board)
+    render_board(board, screen)
 
     pygame.display.update()
